a2ff07c5acc92e4609725c5ff1eb740a21e6d3c4
Shailendra <shailu3597@gmail.com>
2022-04-08 16:50:30 +0530

    fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -284,7 +284,7 @@ class PostController extends Controller
             $post->update(['status' => "Published"]);
                 return response()->json([
                 'message' => 'Post status updated successfully',
-                'status' => "Published",
+                'status' => "published",
                 'post' => $post
             
             ]);
