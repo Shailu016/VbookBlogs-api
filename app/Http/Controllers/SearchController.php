bab9d5369e91c1f3aabc916163d70fd716f2aa3e
Shailendra <shailu3597@gmail.com>
2023-08-17 12:26:21 +0530

    search bug fix

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,20 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\User;
+use auth ;
 
 class SearchController extends Controller
 {
     public function search(Request $request, User $user)
     {
+
         if (!$user) {
+
             return "User not found";
         } else {
             // get user input and search for it
             $search = $request->input('query');
             //search all post of user according to user input match user input with name and body of post
-            $post = Post::where('user_id', $user->id)->where('name', 'like', '%' . $search . '%')->orWhere('body', 'like', '%' . $search . '%')->get();
+            $post = Post::where('user_id', $user->id)->where('name', 'like', '%' . $search . '%')->Where('body', 'like', '%' . $search . '%')->get();
             //return the result
             return response()->json($post);
+
+
+
         }
     }
+
+
 }
